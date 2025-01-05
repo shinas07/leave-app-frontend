7bf58bf9dda70e029ebed57e1cbb10dd7ea1be05
shinas07 <shinasamanpk@gmail.com>
2025-01-05 19:06:48 +0530

    feat: update routes with new pages and components

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,16 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
-import { HomePage } from "../components/HomePage";
 import Login from "../components/auth/Login";
+import ManagerDashboard from "../pages/ManagerDashboard";
+import LandingPage from "../pages/LandingPage";
+import EmployeeDashboard from "../pages/EmployeeDashboard";
+import LeaveApplicationForm from "../components/leave/EmpLeaveApplicationPage";
 
 
 const route = createBrowserRouter([
     {
         path:'/',
-        element:<HomePage/>,
+        element:<LandingPage/>,
     },
     {
         path:'/login',
         element:<Login/>
+    },
+    {
+        path:'/manager/dashboard',
+        element:<ManagerDashboard/>,
+    },
+    {
+        path:'/employee/dashboard',
+        element:<EmployeeDashboard/>
+    },
+    {
+        path:'/employee/leave-form',
+        element:<LeaveApplicationForm/>
     }
 ])
 
