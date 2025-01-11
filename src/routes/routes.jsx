a97dff881ce57f9b89cf9b42378c54718c2dc9ad
shinas07 <shinasamanpk@gmail.com>
2025-01-11 12:04:26 +0530

    refactor(routes): update routes configuration for new pages

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/auth/Login";
-import ManagerDashboard from "../pages/ManagerDashboard";
+import ManagerDashboard from "../pages/ManagerPages/ManagerDashboard";
 import LandingPage from "../pages/LandingPage";
-import EmployeeDashboard from "../pages/EmployeeDashboard";
+import EmployeeDashboard from "../pages/EmployeePages/EmployeeDashboard";
 import LeaveApplicationForm from "../components/leave/EmpLeaveApplicationPage";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+import LeaveHistory from "../components/leave/EmpLeaveHistory";
+import CreateEmployee from "../pages/ManagerPages/CreateEmployee";
+import LeaveRequests from "../pages/ManagerPages/LeaveRequests";
+import AllEmployees from "../pages/ManagerPages/AllEmployees";
+
+const ProtectedRoute = ({ element }) => {
+    const { user} = useAuth()
+  
+    if (user) {
+      // Redirect based on user role
+      return user.user_type === 'manager' ? <Navigate to="/manager/dashboard" /> : <Navigate to="/employee/dashboard" />;
+    }
+
+    return element;
+  };
 
 
 const route = createBrowserRouter([
@@ -13,12 +30,9 @@ const route = createBrowserRouter([
     },
     {
         path:'/login',
-        element:<Login/>
+        element: <ProtectedRoute element={<Login />} />
     },
-    {
-        path:'/manager/dashboard',
-        element:<ManagerDashboard/>,
-    },
+ 
     {
         path:'/employee/dashboard',
         element:<EmployeeDashboard/>
@@ -26,6 +40,26 @@ const route = createBrowserRouter([
     {
         path:'/employee/leave-form',
         element:<LeaveApplicationForm/>
+    },
+    {
+        path: '/employee/leave-history', // Add route for Leave History
+        element: <LeaveHistory />
+    },
+    {
+        path:'/manager/dashboard',
+        element:<ManagerDashboard/>,
+    },
+    {
+        path: '/manager/add-employee', // Add route for Create Employee
+        element:<CreateEmployee/>
+    },
+    {
+        path:'/manager/leave-requests',
+        element:<LeaveRequests/>
+    },
+    {
+        path:'/manager/employees',
+        element:<AllEmployees/>
     }
 ])
 
