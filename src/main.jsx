43f4169520e4afe8a7eebba10f67fdc2ee4b86c7
shinas07 <shinasamanpk@gmail.com>
2025-01-11 12:04:24 +0530

    refactor(main): optimize main entry file for better performance

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'sonner'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Toaster position="top-right" richColors expand={false}/>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </StrictMode>,
 )
